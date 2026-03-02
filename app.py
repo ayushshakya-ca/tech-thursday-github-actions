22b7b085a2d4fbacf3696e32a227001bb6c84547
Srilekha2003 <srilekhakishan2003@gmail.com>
2026-03-02 21:29:05 +0530

    Test new VM runner app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def home():
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>TravelGoa - Explore The World</title>
+<title>TravelG - Explore The World</title>
 
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
 
