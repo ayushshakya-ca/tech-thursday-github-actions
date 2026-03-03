477d2cac3a55fb4f29532d63d2b5a72590fcd6e3
Srilekha2003 <srilekhakishan2003@gmail.com>
2026-03-03 10:27:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def home():
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>TravelG - Explore The World</title>
+<title>TravelX - Explore The World</title>
 
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
 
@@ -263,7 +263,7 @@ footer{
 <body>
 
 <header>
-<div class="logo">✈️ TravelXYZ</div>
+<div class="logo">✈️ TravelX</div>
 
 <nav>
 <ul id="menu">
